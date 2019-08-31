b96faab44d97d329be2ffc1989b09b8a253e4ca1
tcrurav <tiburcio.cruz@gmail.com>
2019-08-31 14:36:15 +0100

    message change

--- controllers/classroom.controller.js
+++ controllers/classroom.controller.js
@@ -88,5 +88,5 @@ exports.classroom_delete = function (req, res) {
 
 //Simple version, without validation or sanitation
 exports.test = function (req, res) {
-    res.send('Greetings from the Test controller!');
+    res.send('Greetings from the Test controller 1!');
 };
